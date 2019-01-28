5e9dd0372f7d0031c5905565bc78283856db5f11
zhen8838 <597323109@qq.com>
2019-01-28 13:38:45 +0800

    rm

--- mobilenetv1/base_func.py
+++ mobilenetv1/base_func.py
@@ -358,7 +358,7 @@ def load_model(model, input_map=None):
     model_exp = os.path.expanduser(model)
     if (os.path.isfile(model_exp)):
         print('Model filename: %s' % model_exp)
-        with gfile.FastGFile(model_exp, 'rb') as f:
+        with gfile.GFile(model_exp, 'rb') as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
             tf.import_graph_def(graph_def, input_map=input_map, name='')
--- testpy.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from mobilenetv1.base_func import *
-
-
-if __name__ == "__main__":
-    g = tf.get_default_graph()
-    sess = tf.Session()
-    load_model('pretrained/mobilenetv1_1.0.pb')
-    g.get_operations()
